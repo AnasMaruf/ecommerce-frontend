6e7d59e6c746085a6ef0343f5c5e1d2ad94909b1
AnasMaruf <anasmaruf1101@gmail.com>
2024-12-18 09:54:04 +0700

    Add pop up review

--- components/base/Tabs.vue
+++ components/base/Tabs.vue
@@ -1,47 +1,47 @@
 <template>
-  <UTabs
-    v-model="tabActive"
-    :items="item"
-    :ui="{
-      list: {
-        background: 'bg-white border-b',
-        padding: 'p-0',
-        rounded: 'rounded-none',
-        marker: {
-          wrapper: '!top-[0px] left-0 !h-[100%] border-b-2 border-primary',
-          rounded: 'rounded-none',
-          shadow: 'shadow-none',
-        },
-        tab: {
-          active: 'text-primary',
-          inactive: 'text-black/80',
-          rounded: 'rounded-none',
-        },
-      },
-    }"
-  >
-    <template #item="{ item }">
-      <template v-if="options[item.key].length">
-        <UButton
-          v-for="child in options[item.key]"
-          :key="child.uuid"
-          variant="ghost"
-          block
-          :color="form[item.key]?.uuid === child.uuid ? 'primary' : 'gray'"
-          class="justify-start"
-          :label="child.name"
-          @click="handleSelect(child, item.key, close)"
-        />
-      </template>
-      <p v-else class="text-center my-3 text-black/40">
-        Tidak ada data yang ditemukan
-      </p>
+  <UTabs v-model="tabActive" :items="item" :ui="customUi">
+    <template #item="slotScope">
+      <slot name="item" v-bind="slotScope" />
     </template>
   </UTabs>
 </template>
 
-<script>
-export default {};
+<script setup>
+import defu from "defu";
+const tabActive = defineModel({
+  type: Number,
+  default: 0,
+});
+
+const props = defineProps({
+  item: {
+    type: Array,
+    default: () => [],
+  },
+  ui: {
+    type: Object,
+    default: () => {},
+  },
+});
+
+const defaultUi = {
+  list: {
+    background: "bg-white border-b",
+    padding: "p-0",
+    rounded: "rounded-none",
+    marker: {
+      wrapper: "!top-[0px] left-0 !h-[100%] border-b-2 border-primary",
+      rounded: "rounded-none",
+      shadow: "shadow-none",
+    },
+    tab: {
+      active: "text-primary",
+      inactive: "text-black/80",
+      rounded: "rounded-none",
+    },
+  },
+};
+const customUi = computed(() => defu(props.ui, defaultUi));
 </script>
 
 <style lang="scss" scoped></style>
---
+++ components/feature/ProfileOrder/CardProduct.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="flex gap-3">
+    <img
+      src="https://picsum.photos/1920/1000?random=1"
+      class="aspect-[1/1] w-20 h-20 object-cover"
+      :class="sizeClasses[size]?.image"
+    />
+    <div class="flex-1">
+      <p class="line-clamp-2" :class="sizeClasses[size]?.title">Product Name</p>
+      <template v-if="!hideDescription">
+        <p class="text-sm text-black/55">Variasi: white, 39</p>
+        <p class="text-sm">x1</p>
+      </template>
+    </div>
+    <div v-if="!hidePrice" class="flex items-center">
+      Rp.{{ formatNumber(100000) }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  hidePrice: {
+    type: Boolean,
+    default: false,
+  },
+  hideDescription: {
+    type: Boolean,
+    default: false,
+  },
+  size: {
+    type: String,
+    default: "lg",
+    validator: (propsValue) => ["sm", "lg"].includes(propsValue),
+  },
+});
+
+const sizeClasses = {
+  sm: {
+    image: "h-14 w-14",
+    title: "text-sm",
+  },
+  lg: {
+    image: "h-20 w-20",
+    title: "text-base",
+  },
+};
+</script>
+
+<style scoped></style>
