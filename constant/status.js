9d858fbde60dedc77ec8ef35052b14b2312fa995
AnasMaruf <anasmaruf1101@gmail.com>
2025-02-09 23:52:32 +0700

    Integrasi Order List (2)

---
+++ constant/status.js
@@ -0,0 +1,8 @@
+export const STATUS_ORDER = {
+  pending_payment: "Belum bayar",
+  paid: "Sudah dibayar",
+  on_processing: "Sedang dikemas",
+  on_delivery: "Dikirim",
+  done: "Selesai",
+  failed: "Dibatalkan",
+};
--- pages/my-account/orders/index.vue
+++ pages/my-account/orders/index.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="space-y-3">
     <BaseTabs
+      v-model="formFilter.status"
       :items="items"
       :ui="{
         list: {
           height: 'h-full',
-          tab: { font: 'font-normal', height: 'h-14' },
+          tab: {
+            font: 'font-normal',
+            height: 'h-14',
+          },
         },
       }"
       :content="false"
     />
     <UInput
+      v-model="formFilter.search"
       leading-icon="i-heroicons:magnifying-glass"
       size="lg"
-      placeholder="Kamu bisa cari berdasarkan nama penjual, No. Pesanan, atau nama produk"
+      placeholder="Kamu bisa cari berdasarkan Nama Penjual, No. Pesanan atau Nama Produk"
       :ui="{
         color: {
           white: {
@@ -22,155 +27,25 @@
         },
       }"
     />
-    <UCard
-      v-for="i in 4"
-      :key="`order-${i}`"
-      :ui="{
-        base: 'text-sm text-black/85',
-        divide: 'divide-dashed',
-        footer: {
-          background: 'bg-yellow-50/30',
-        },
-      }"
-    >
-      <template #default>
-        <div class="flex justify-between gap-4 items-center">
-          <div class="flex gap-2 items-center">
-            <span class="font-semibold">SHOP NAME</span>
-            <UButton color="white" size="xs" :to="`/shop/sellerusername`"
-              ><IconShop /> Kunjungi Toko</UButton
-            >
-          </div>
-          <div class="divide-x flex">
-            <div class="flex gap-2 items-center text-teal-500 pr-2">
-              <UIcon name="i-heroicons:truck" class="w-4 h-4" />
-              <span>Ini nanti deskripsi dari paket yang telah sampai</span>
-            </div>
-            <span class="text-primary pl-2">Selesai</span>
-          </div>
-        </div>
-        <hr class="my-4" />
-        <NuxtLink class="space-y-3" to="/my-account/orders/order-id">
-          <FeatureProfileOrderCardProduct v-for="i in 3" :key="i" />
-        </NuxtLink>
-      </template>
-      <template #footer>
-        <div class="space-y-6">
-          <div class="flex justify-end">
-            <div class="flex gap-2 items-center">
-              <img src="~/assets/images/garansi.png" class="w-4 h-4" />
-              <span class="text-black/80">Total Pesanan:</span>
-              <span class="text-primary text-2xl"
-                >Rp.{{ formatNumber(100000) }}</span
-              >
-            </div>
-          </div>
-          <div class="flex justify-between items-center gap-4">
-            <div class="text-xs">
-              <p class="text-black/55">
-                Nilai produk sebelum <span class="underline">31-10-2024</span>
-              </p>
-              <p class="text-primary">
-                Nilai sekarang & dapatkan 25 koin syopo!
-              </p>
-            </div>
-            <UButton
-              label="Nilai"
-              class="min-w-40 justify-center"
-              @click="openReview = true"
-            />
-          </div>
-        </div>
-      </template>
-    </UCard>
+    <FeatureProfileOrderCardOrder
+      v-for="(order, index) in data?.data || []"
+      :key="`order-${index}`"
+      :order="order"
+      @review="handleReview"
+    />
+    <div ref="observerElement" />
+    <BaseLoading v-if="status === 'pending'" />
+    <template v-else-if="(data?.data || []).length === 0">
+      <p class="text-sm font-medium text-black/55 py-4 text-center">
+        Tidak ada order yang ditemukan
+      </p>
+    </template>
 
-    <UModal v-model="openReview">
-      <UCard class="text-black/85">
-        <p class="text-xl font-normal">Nilai Produk</p>
-        <div class="space-y-5 mt-8">
-          <div
-            class="border border-yellow-500 bg-yellow-50 rounded-sm py-2 px-3 flex gap-2"
-          >
-            <IconCoinSolid />
-            <span class="font-medium">Beri penilaian & dapatkan 25 koin!</span>
-          </div>
-          <div class="space-y-2">
-            <FeatureProfileOrderCardProduct
-              v-for="a in 3"
-              :key="`Product-${a}`"
-              hide-price
-              hide-description
-              size="sm"
-            />
-          </div>
-          <div class="flex gap-2 items-center">
-            <span class="w-44">Kualitas Produk</span>
-            <BaseRating v-model="formRating.rating" size="lg" color="yellow" />
-            <span
-              :class="{
-                'text-yellow-500': formRating.rating > 3,
-                'text-black/55': formRating.rating <= 3,
-              }"
-              >{{ reviewMessages[formRating.rating - 1] }}</span
-            >
-          </div>
-          <div>
-            <div class="bg-gray-100 px-6 py-3 rounded-sm space-y-3">
-              <UTextarea
-                placeholder="Bagikan penilaianmu tentang produk ini untuk membantu pembeli lain"
-              />
-              <div class="flex gap-2">
-                <UButton
-                  size="xs"
-                  variant="soft"
-                  icon="i-heroicons:camera-solid"
-                  label="Tambah Foto"
-                />
-                <UButton
-                  size="xs"
-                  variant="soft"
-                  icon="i-heroicons:video-camera-solid"
-                  label="Tambahkan Video"
-                />
-              </div>
-              <p class="text-right text-black/55">
-                Tambahkan 50 karakter dengan 1 foto untuk mendapatkan 25 koin!
-              </p>
-            </div>
-          </div>
-          <div class="space-y-4">
-            <p class="text-base">Tentang Layanan</p>
-            <div class="flex gap-2 items-center">
-              <span class="w-44">Kecepatan jasa kirim</span>
-              <BaseRating
-                v-model="formRating.courierRating"
-                size="lg"
-                color="yellow"
-              />
-              <span
-                :class="{
-                  'text-yellow-500': formRating.courierRating > 3,
-                  'text-black/55': formRating.courierRating <= 3,
-                }"
-                >{{ reviewMessages[formRating.courierRating - 1] }}</span
-              >
-            </div>
-          </div>
-          <div class="flex justify-end gap-2 pt-6">
-            <UButton
-              class="min-w-36 justify-center"
-              variant="link"
-              color="gray"
-              @click="openReview = false"
-              >Nanti Saja</UButton
-            >
-            <UButton class="min-w-36 justify-center" @click="openReview = false"
-              >OK</UButton
-            >
-          </div>
-        </div>
-      </UCard>
-    </UModal>
+    <ModalReview
+      ref="modalReviewElement"
+      v-model:open="openReview"
+      @success="handleReloadData"
+    />
   </div>
 </template>
 
@@ -178,19 +53,27 @@
 definePageMeta({
   wrapper: "div",
 });
+const { data: oldOrders } = useNuxtData("order-customer");
 
 const openReview = ref(false);
-const formRating = reactive({
-  rating: 0,
-  courierRating: 0,
+const modalReviewElement = ref();
+
+const observerElement = ref();
+let observer = null;
+
+const pagination = ref({
+  page: 1,
+  per_page: 3,
 });
-const reviewMessages = [
-  "Sangat Buruk",
-  "Buruk",
-  "Biasa",
-  "Baik",
-  "Sangat Baik",
-];
+const formFilter = ref({
+  search: "",
+  status: 0,
+});
+
+const searchDebounce = refDebounced(
+  computed(() => formFilter.value.search),
+  1000
+);
 
 const items = [
   {
@@ -200,6 +83,10 @@ const items = [
   {
     label: "Belum Bayar",
     key: "pending_payment",
+  },
+  {
+    label: "Sudah Dibayar",
+    key: "paid",
   },
   {
     label: "Sedang Dikemas",
@@ -218,6 +105,77 @@ const items = [
     key: "failed",
   },
 ];
+
+const { data, status } = useApi("/server/api/order", {
+  key: "order-customer",
+  onResponse({ response }) {
+    if (response.ok) {
+      if (!observer) {
+        observer = new IntersectionObserver((entries) => {
+          if (
+            entries[0].isIntersecting &&
+            !!response._data.data.next_page_url &&
+            pagination.value.page < response._data.data.last_page
+          ) {
+            pagination.value.page++;
+          }
+        });
+        observer.observe(observerElement.value);
+      }
+    }
+  },
+  params: computed(() => {
+    const lastStatus =
+      formFilter.value.status > 0
+        ? items[formFilter.value.status]?.key
+        : undefined;
+    return {
+      ...pagination.value,
+      search: searchDebounce.value || undefined,
+      last_status: lastStatus,
+    };
+  }),
+  transform(response) {
+    if (pagination.value.page === 1) return response?.data;
+    const newData = response?.data?.data || [];
+    return {
+      ...response.data,
+      data: [...(oldOrders.value?.data || []), ...newData],
+    };
+  },
+});
+
+onUnmounted(() => {
+  if (observer) {
+    observer.disconnect();
+    observer = null;
+  }
+});
+
+watch(searchDebounce, () => {
+  pagination.value.page = 1;
+});
+
+watch(
+  () => formFilter.value.status,
+  () => {
+    if (observer) {
+      observer.disconnect();
+      observer = null;
+    }
+    formFilter.value.search = "";
+    pagination.value.page = 1;
+  }
+);
+
+function handleReview(order) {
+  modalReviewElement.value.setDefaultData(order);
+  openReview.value = true;
+}
+
+function handleReloadData() {
+  refreshNuxtData("order-customer");
+}
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
